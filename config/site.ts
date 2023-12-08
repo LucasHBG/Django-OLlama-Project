b77155e8daa86ad7b13c94952a16c6a3b181e239
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2023-12-08 13:29:41 -0300

    feat: site reusable info

---
+++ config/site.ts
@@ -0,0 +1,16 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "West Chat",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    "whatsapp": "https://wa.me/5562982560169"
+  }
+}
