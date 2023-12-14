adc3d0547e1d4fbe7971686e74ddd2f81270f12a
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2023-12-14 18:46:13 -0300

    feat: added login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,53 @@
-export default function Login() {
+export default function LoginPage() {
     return (
-        <main>
-            <p>Hola!</p>
+        <main className="flex justify-center items-center pb-8 pt-6 md:py-10 mx-4">
+            <div className="rounded-lg border bg-card text-card-foreground shadow-sm">
+                <div className="flex flex-col space-y-1.5 p-6">
+                    <h2 className="font-semibold tracking-tight text-2xl">
+                        Login
+                    </h2>
+                    <p className="text-sm text-muted-foreground">
+                        Insira suas informações para ter acesso ao dashboard.
+                    </p>
+                </div>
+                <div className="p-6 space-y-4">
+                    <div className="space-y-2">
+                        <label
+                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            htmlFor="email"
+                        >
+                            E-mail
+                        </label>
+                        <input
+                            className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                            id="email"
+                            placeholder="admin@westside.com"
+                            required
+                            type="email"
+                        />
+                    </div>
+                    <div className="space-y-2">
+                        <label
+                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                            htmlFor="password"
+                            >
+                            Senha
+                        </label>
+                        <input
+                            className="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                            id="password"
+                            placeholder="***************"
+                            required
+                            type="password"
+                        />
+                    </div>
+                </div>
+                <div className="flex items-center p-6">
+                    <button className="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2 w-full">
+                        Entrar
+                    </button>
+                </div>
+            </div>
         </main>
     )
 }
