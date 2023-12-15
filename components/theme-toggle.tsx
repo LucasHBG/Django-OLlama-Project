ebb03193f04b1edbeed1e0fe99d68b9f0117de61
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2023-12-15 17:26:13 -0300

    chore: refactoring theme-toggle component

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,31 +1,28 @@
-'use client'
+"use client"
 
-import * as React from 'react'
-import { useTheme } from 'next-themes'
+import * as React from "react"
+import { useTheme } from "next-themes"
 
-import { Button } from '@/components/ui/button'
-import { IconMoon, IconSun } from '@/components/icons'
+import { Button } from "@/components/ui/button"
+import { IconMoon, IconSun } from "@/components/icons"
 
 export function ThemeToggle() {
-  const { setTheme, theme } = useTheme()
-  const [_, startTransition] = React.useTransition()
+    const { setTheme, theme } = useTheme()
+    const [_, startTransition] = React.useTransition()
 
-  return (
-    <Button
-      variant="ghost"
-      size="icon"
-      onClick={() => {
-        startTransition(() => {
-          setTheme(theme === 'light' ? 'dark' : 'light')
-        })
-      }}
-    >
-      {!theme ? null : theme === 'dark' ? (
-        <IconMoon className="transition-all" />
-      ) : (
-        <IconSun className="transition-all" />
-      )}
-      <span className="sr-only">Toggle theme</span>
-    </Button>
-  )
+    return (
+        <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => {
+                startTransition(() => {
+                    setTheme(theme === "light" ? "dark" : "light")
+                })
+            }}
+        >
+                <IconMoon className="h-5 w-5 transition-all dark:hidden" />
+                <IconSun className="h-5 w-5 transition-all hidden dark:block" />
+                <span className="sr-only">Toggle theme</span>
+        </Button>
+    )
 }
