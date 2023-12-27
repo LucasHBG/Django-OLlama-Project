f6a90f36444c8753b372cb5ceb53a7584268934e
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2023-12-27 16:06:21 -0300

    feat: new routes

---
+++ utils/client-routes.ts
@@ -0,0 +1,10 @@
+const baseUrl = ""
+
+export const routeLogin = `${baseUrl}/login`
+
+/**
+ *  Authenticated only routes
+ */
+const routeAuth = `${baseUrl}/auth`
+
+export const routeDashboard = `${routeAuth}/dashboard`
