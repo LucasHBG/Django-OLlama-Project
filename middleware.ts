4dc5b85b3a81663be18beef6369ee8dd8f1e1a89
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2023-12-28 01:01:13 -0300

    chore: added composition

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { NextResponse, type NextRequest } from "next/server"
+import { wscAuthCookie } from "./utils/cookies"
 
 export const middleware = (req: NextRequest) => {
     /**
@@ -6,7 +7,7 @@ export const middleware = (req: NextRequest) => {
      * then compare to the token
      * */
     const isValid =
-        (req.headers.get("Authorization") ?? "") === "wscompany-token"
+        (req.headers.get("Authorization") ?? "") === wscAuthCookie
 
     if (!isValid) return NextResponse.redirect(new URL("/login", req.url))
 }
