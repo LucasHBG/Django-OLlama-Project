64c2b1e2c453bf9e215966420cbb7252a6d6d172
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2023-12-30 17:24:52 -0300

    feat: new entries

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,24 @@
 import { NextResponse, type NextRequest } from "next/server"
+
 import { wscAuthCookie } from "./utils/cookies"
 
 export const middleware = (req: NextRequest) => {
+
     /**
      * If there is an Authorization header on the request
      * then compare to the token
      * */
-    const isValid =
-        (req.headers.get("Authorization") ?? "") === wscAuthCookie
+    const isValid = (req.headers.get("Authorization") ?? "") === wscAuthCookie
 
     if (!isValid) return NextResponse.redirect(new URL("/login", req.url))
 }
 
 export const config = {
-    // Every other route except the ones at /auth/* won't run with the middleware.
-    matcher: ["/auth/:path*"],
+    // Every other route except the ones listed here won't run with the middleware.
+    //! Dont forget to get a rule that accepts requests outside
+    //! authenticated layers like /api/login, /api/signup etc
+    matcher: [
+        "/auth/:path*",
+        "/api/auth/:path*"
+    ],
 }
