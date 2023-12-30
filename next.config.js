fa87984e1c2338b8003bf2b3f178cee6463ab5ba
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2023-12-30 17:24:29 -0300

    chore: routes header modifications

--- next.config.js
+++ next.config.js
@@ -1,6 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    async headers() {
+        return [
+            //? All routes listed here can be accessed by every origin
+            {
+                source: "/",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,DELETE,PATCH,POST,PUT",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
+                ],
+            },
+            //? Here you can add custom origins to allow only some origins to access
+            {
+                source: "/api/auth/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    {
+                        key: "Access-Control-Allow-Methods",
+                        value: "GET,DELETE,PATCH,POST,PUT",
+                    },
+                    {
+                        key: "Access-Control-Allow-Headers",
+                        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                    },
+                ],
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
