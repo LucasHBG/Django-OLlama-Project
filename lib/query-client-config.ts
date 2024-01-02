79bdd32ae37f0a98a700bde901d5e8cb2299d45f
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-01-02 17:34:42 -0300

    feat: added react query config

---
+++ lib/query-client-config.ts
@@ -0,0 +1,13 @@
+"use client"
+
+import { QueryClient } from "@tanstack/react-query"
+
+const queryClientConfig = new QueryClient({
+    defaultOptions: {
+        queries: {
+            refetchOnWindowFocus: false,
+        },
+    },
+})
+
+export default queryClientConfig;
