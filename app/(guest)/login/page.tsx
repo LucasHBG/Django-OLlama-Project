c5a5081c9db3568fa97d444f84724c2ba4de715b
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-01-03 16:01:03 -0300

    chore: moved auth folder to new group

---
+++ app/(guest)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/login-form";
+
+export default function LoginPage() {
+    return (
+        <main className="flex min-h-screen items-start justify-center bg-gradient-to-b from-white to-gray-900 px-4 pb-8 pt-6 dark:from-gray-900 dark:to-white md:py-10">
+            <LoginForm />
+        </main>
+    )
+}
