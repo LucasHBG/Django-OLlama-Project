8f753daae211eab9e7ed9e633916a8453cebc912
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-01-03 16:01:19 -0300

    chore: moved files

--- app/admin/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Admin() {
-    return (
-        <main>
-            <h2>Área do admin</h2>
-        </main>
-    )
-}
--- app/auth/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function DashboardPage() {
-    return <main>Hello mina-san</main>
-}
