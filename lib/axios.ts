f041ac35950e3300f5627bc4fe660855653a6638
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-01-03 14:26:49 -0300

    feat: added interceptors at axios

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,5 +1,7 @@
+import { wscAuthCookie } from "@/utils/cookies"
 import { apiURL } from "@/utils/server-routes"
-import Axios, { AxiosError, AxiosResponse } from "axios"
+import Axios, { AxiosError, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from "axios"
+import Cookies from "js-cookie"
 
 const axios = Axios.create({
     baseURL: apiURL,
@@ -8,6 +10,21 @@ const axios = Axios.create({
         "X-Requested-With": "XMLHttpRequest",
     },
 })
+
+// O interceptador de chamadas http chama essa função em todas as requisições.
+const setAuthorizationHeader = (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+    const token = Cookies.get(wscAuthCookie)
+
+    config.headers = config.headers ?? {}
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config
+}
+
+axios.interceptors.request.use(setAuthorizationHeader)
 
 axios.interceptors.response.use(
     (response: AxiosResponse) => {
