fca56f3d92886fc6e073835d356cccbdb1721f84
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-01-03 16:00:54 -0300

    chore: moved auth folder to new group

---
+++ app/(authenticated)/auth/dashboard/page.tsx
@@ -0,0 +1,18 @@
+export default function DashboardPage() {
+    return (
+        <main className="p-10 space-y-4">
+            <h2 className="text-lg font-medium">
+                Aqui você visualiza as conversas do seu WhatsApp
+            </h2>
+
+            <p className="text-sm font-normal text-gray-700 dark:text-gray-400">
+                Você pode escolher a data de início e fim para filtrar as
+                conversas.
+            </p>
+
+            <p className="text-sm font-normal text-gray-700 dark:text-gray-400">
+                Logo abaixo sairá o resultado da pergunta no WhatsApp:
+            </p>
+        </main>
+    )
+}
