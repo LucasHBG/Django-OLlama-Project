8d39220dbba9fc3ad1142b6e00a4222de746e543
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-01-05 16:44:10 -0300

    feat: add toaster to layout authenticated

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata, Viewport } from "next"
 import "@/styles/globals.css"
 
 import { QueryClientProvider } from "@tanstack/react-query"
+import { Toaster } from "react-hot-toast"
 
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
@@ -44,6 +45,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                     fontSans.variable
                 )}
             >
+                <Toaster />
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
