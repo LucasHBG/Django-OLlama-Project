4b79e969b4ef4ea9b7c31e1deefb3cb0348b1a9b
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-03-18 09:23:58 -0300

    feat: added new tailwind property

--- app/(guest)/page.tsx
+++ app/(guest)/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-dvh flex-col items-center justify-between p-24">
             <p>landing page</p>
         </main>
     )
