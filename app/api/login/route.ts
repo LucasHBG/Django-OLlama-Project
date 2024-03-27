4927fbce87dfeb5ebb7c68935942333a23b2c8d8
Lucas H. B. Guimaraes <lukahbg@gmail.com>
2024-03-27 10:37:50 -0300

    feat: using zod to validate login object with zod

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,6 +1,8 @@
 import { cookies } from "next/headers"
 import { NextRequest, NextResponse } from "next/server"
-import { wscAuthCookie } from "@/utils/cookies"
+import { wscAuthCookie } from "@/lib/cookies"
+
+import { loginFormSchema } from "@/lib/validations/form"
 
 // Notice the funciton definiton:
 export async function GET(req: NextRequest) {
@@ -17,6 +19,15 @@ export async function POST(req: NextRequest, res: NextResponse) {
         const data = await new Response(req.body).json()
         const email = data["email"]
         const password = data["password"]
+
+        const parsedLogin = loginFormSchema.safeParse({
+            email: email,
+            password: password,
+        })
+
+        if (!parsedLogin.success) {
+            return NextResponse.json(parsedLogin.error, { status: 422 })
+        }
 
         cookies().set(wscAuthCookie, `S2$212345s12`)
 
